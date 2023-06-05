17bf7affd95e04afb9b33b00a3f0ff1b3cfdc1d6
Dawid4678 <Dawid4678>
2023-06-05 19:55:01 +0200

    dodanie sub do kalkulatora

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public int add(int x, int y){
         return x + y;
     }
+
+    public double sub(double x, double y) {
+        return x - y;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add(4,7));
+        System.out.println(calculator.add(7,4));
+        System.out.println(calculator.sub(7,4));
     }
 }
