3718a3013a53034a90567a07b6e40f080d0742aa
Dawid4678 <Dawid4678>
2023-06-06 18:00:46 +0200

    bank app

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,12 +1,22 @@
 package org.example;
 
 public class Bank {
-    public double bilans = 0;
-    public double depozyt(double wplata){
 
+    public double bilans = 0;
+    public double getBilans(){
+        return bilans;
+    }
+    public double depozyt(double wplata){
         return bilans = bilans + wplata ;
     }
     public double wyplata(double kasa){
-        return bilans = bilans - kasa;
+        if (bilans < kasa){
+            System.out.println("nic z tego :p");
+        }
+        else {
+             bilans = bilans - kasa;
+
+        }
+        return bilans;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Bank bank = new Bank();
         bank.depozyt(500);
@@ -10,13 +11,11 @@ public class Main {
         System.out.println("Podaj swoje imię");
         Scanner scanneruser = new Scanner(System.in);
         String user=scanneruser.nextLine();
-        System.out.println("Witaj ! "+user );
+        System.out.println("Witaj ! " + user);
         System.out.println("Ile pieniędzy chciałbyś wypłacić ?");
         Scanner scanner = new Scanner(System.in);
         double kasa=scanner.nextDouble();
-        boolean isCorrect= bank.bilans - bank.wyplata(kasa) >=0;
-        System.out.println("Bilans " + bank.bilans);
-        boolean isNotCorrect= bank.bilans - bank.wyplata(kasa) <=0;
-        System.out.println("nic z tego :p");
+        bank.wyplata(kasa);
+        System.out.println("Bilans: " + bank.getBilans());
     }
 }
