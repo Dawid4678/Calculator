8fa34c18a267714c9d886a3e7f9f62da977ce95e
Dawid4678 <Dawid4678>
2023-06-06 17:22:41 +0200

    bank app

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,11 +1,12 @@
 package org.example;
 
 public class Bank {
-
-    double bilans = 0;
+    public double bilans = 0;
     public double depozyt(double wplata){
+
         return bilans = bilans + wplata ;
     }
-    
-    
+    public double wyplata(double kasa){
+        return bilans = bilans - kasa;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,22 @@
 package org.example;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
-        bank.depozyt(1000);
-        System.out.println(bank.depozyt(1000));
+        bank.depozyt(500);
+        System.out.println("Witaj w naszym banku!");
+        System.out.println("Podaj swoje imię");
+        Scanner scanneruser = new Scanner(System.in);
+        String user=scanneruser.nextLine();
+        System.out.println("Witaj ! "+user );
+        System.out.println("Ile pieniędzy chciałbyś wypłacić ?");
+        Scanner scanner = new Scanner(System.in);
+        double kasa=scanner.nextDouble();
+        boolean isCorrect= bank.bilans - bank.wyplata(kasa) >=0;
+        System.out.println("Bilans " + bank.bilans);
+        boolean isNotCorrect= bank.bilans - bank.wyplata(kasa) <=0;
+        System.out.println("nic z tego :p");
     }
 }
